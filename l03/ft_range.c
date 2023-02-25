6a84f2b4243d00a372b9fe2bc0357877cf162d46
Alice NIEZGODZKI <aniezgod@e2r12p23.clusters.42paris.fr>
2023-02-25 17:58:55 +0100

    maj

--- l03/ft_range.c
+++ l03/ft_range.c
@@ -6,47 +6,40 @@
 /*   By: aniezgod <aniezgod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 17:26:10 by aniezgod          #+#    #+#             */
-/*   Updated: 2023/01/11 18:10:34 by aniezgod         ###   ########.fr       */
+/*   Updated: 2023/02/25 17:58:27 by aniezgod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int	ft_array_size(int start, int end)
+int ft_strlen(int a, int b)
 {
-	int len = 1;
+    int min = a;
+    int max = b;
 
-	if (start == end)
-		len = 1;
-	while (start != end)
-	{
-		if (start > end)
-			end++;
-		else if (start < end)
-			start++;
-		len++;
-	}
-	return (len);
-	
+    if (min > max)
+    {
+        min = b;
+        max = a;
+    }
+    return(max -  min + 1);
 }
 
-int	*ft_range(int start, int end)
+int *ft_range(int start, int end)
 {
-	int size;
-	int i = 0;
-	int *array = NULL;
+    int *tab;
+    int i = 0;
+    int size;
 
-	size = ft_array_size(start, end);
-	array = malloc(sizeof(int *) * size);
-	if (!array)
-		return (NULL);
-	while (i < size)
-	{
-		if (start > end)
-			array[i] = start - i;
-		else
-			array[i] = start + i;
-		i++;
-	}
-	return(array);	
+    size = ft_strlen(start, end);
+    tab = malloc(sizeof(int) * size);
+    if (!tab)
+        return (NULL);
+    if (start < end)
+        while (i < size)
+            tab[i++] = start++;
+    else
+        while(i < size)
+            tab[i++] = start--;
+    return (tab);
 }
